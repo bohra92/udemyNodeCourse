44f6afb0e444b5797ede7483dc21ecf61f10f0aa
bohra92 <bohratanuj@gmail.com>
2019-09-10 17:14:12 +0530

    2nd commit adding a new branch and basics folder

---
+++ Basics/exportingModules/example1/app.js
@@ -0,0 +1,11 @@
+const calculate = require('./calculate.js')
+
+console.log('This is app.js')
+
+console.log('Adding 6 & 2 : ', calculate.add(6, 2))
+
+console.log('Subtracting 6 & 2 : ', calculate.sub(6, 2))
+
+console.log('Multiplying 6 & 2 : ', calculate.mult(6, 2))
+
+console.log('Dividing 6 & 2 : ', calculate.div(6, 2))
