bad5eb5db4c536c55fd1028aeba151f7cbe970a3
bohra92 <bohratanuj@gmail.com>
2019-09-16 18:47:26 +0530

    added files in Basics

---
+++ Basics/chalkModule/app.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+
+console.log(chalk.blue('Success'));
+console.log(chalk.green('Failure'));
+
+console.log(chalk.inverse.blue('Success'));
+console.log(chalk.bold.inverse.green('Failure'));
